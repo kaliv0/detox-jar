fcdcbca616fcbe39a8c7f1f7aa93815fe30e1954
kaliv0 <kaloyan.ivanov88@gmail.com>
2024-09-26 19:43:49 +0300

    refactored code, added cli args, added error logs and stats

--- detox/__main__.py
+++ detox/__main__.py
@@ -1,3 +1,25 @@
-from detox.runner import main
+import argparse
 
-main()
+from detox.runner import DetoxRunner
+from detox import __version__
+
+
+def get_command_line_args():
+    parser = argparse.ArgumentParser(
+        prog="detox",
+        description="Automation tool",
+    )
+
+    parser.add_argument("--version", action="version", version=f"%(prog)s v{__version__}")
+    parser.add_argument(
+        "-j",
+        "--job",
+        help="pick a job from detox.toml file to run",
+    )
+    return parser.parse_args()
+
+
+# TODO: remove
+if __name__ == "__main__":
+    args = get_command_line_args()
+    DetoxRunner().run(args)
--- detox/runner.py
+++ detox/runner.py
@@ -3,62 +3,160 @@ import subprocess
 import sys
 import time
 import tomllib
+from itertools import chain
 
 from detox.logger import DetoxicoLogger
 
 
 class DetoxRunner:
-    CONFIG_FILE = "../detox.toml"
+    CONFIG_FILE = "./detox.toml"
     TMP_VENV = ".detoxenv"
 
     def __init__(self):
         self.data = None
         self.all_jobs = []
+        self.successful_jobs = []
         self.failed_jobs = []
 
-    def suite(self):
+        self.is_toml_file_valid = False
+        self.is_setup_successful = False
+        self.is_teardown_successful = False
+        self.is_detox_successful = True  # needed for bitwise &
+
+    @property
+    def skipped_jobs(self):
+        return [
+            job for job in self.all_jobs if job not in chain(self.failed_jobs, self.successful_jobs)
+        ]
+
+    @property
+    def job_suite(self):
         if "run" in self.data:
             jobs = dict(self.data["run"].items())
             if "suite" not in jobs:
-                raise KeyError("missing key 'suite' in 'run' table")
-
+                DetoxicoLogger.error("Encountered error: missing key 'suite' in 'run' table")
+                return None
             self.all_jobs = jobs["suite"]
             return ((k, self.data[k]) for k in jobs["suite"])
         else:
             self.all_jobs = list(self.data.keys())
             return self.data.items()
 
-    def detox(self):
-        is_detox_successful = True
+    # main flow
+    def run(self, args):
+        global_start = time.perf_counter()
+        DetoxicoLogger.info("Detoxing begins:")
 
-        for key, val in self.suite():
+        self._run_detox_stages(args)
+
+        global_stop = time.perf_counter()
+        if self.is_detox_successful:
+            DetoxicoLogger.info(f"All jobs succeeded! {self.successful_jobs}")
+            DetoxicoLogger.info(f"Detoxing took: {global_stop - global_start}")
+        else:
+            DetoxicoLogger.fail(f"Unsuccessful detoxing took: {global_stop - global_start}")
+            DetoxicoLogger.error(f"Failed jobs: {self.failed_jobs}")
+            DetoxicoLogger.info(
+                f"Successful jobs: {[x for x in self.successful_jobs if x not in self.failed_jobs]}"
+            )
+            if self.skipped_jobs:
+                DetoxicoLogger.fail(f"Skipped jobs: {self.skipped_jobs}")
+
+    def _run_detox_stages(self, args):
+        self._read_file()
+        if not self.is_toml_file_valid:
+            DetoxicoLogger.fail("Detoxing failed: missing or empty detox.toml file")
+            sys.exit(1)
+
+        if not self._validate_args(args):
+            DetoxicoLogger.fail(f"Detoxing failed: '{args.job}' not found in detox.toml jobs")
+            sys.exit(1)
+
+        self._setup()
+        if not self.is_setup_successful:
+            DetoxicoLogger.fail("Detoxing failed :(")
+            sys.exit(1)
+
+        self._run_jobs()
+
+        for _ in range(3):
+            self._teardown()
+            if self.is_teardown_successful:
+                break
+
+    def _read_file(self):
+        if not os.path.exists(self.CONFIG_FILE) or os.path.getsize(self.CONFIG_FILE) == 0:
+            self.is_toml_file_valid = False
+            return
+
+        with open(self.CONFIG_FILE, "rb") as f:
+            self.data = tomllib.load(f)
+        self.is_toml_file_valid = bool(self.data)
+
+    def _validate_args(self, args):
+        if args.job is None:
+            return True
+        return args.job in self.data
+
+    # setup environment
+    def _setup(self):
+        DetoxicoLogger.log("Creating venv...")
+        prepare = f"python3 -m venv {self.TMP_VENV}"
+        is_successful = self._run_subprocess(prepare)
+        if is_successful:
+            self.is_setup_successful = True
+        else:
+            DetoxicoLogger.error("Failed creating new virtual environment")
+            self.is_setup_successful = False
+
+    def _teardown(self):
+        teardown = f"rm -rf {self.TMP_VENV}"
+        DetoxicoLogger.log("Removing venv...")
+        is_successful = self._run_subprocess(teardown)
+        if is_successful:
+            self.is_teardown_successful = True
+        else:
+            DetoxicoLogger.error("Failed removing virtual environment")
+            self.is_teardown_successful = False
+
+    def _run_jobs(self):
+        if not self.job_suite:
+            self.is_detox_successful = False
+            return
+
+        for table, table_entries in self.job_suite:
             DetoxicoLogger.log("#########################################")
-            DetoxicoLogger.start(f"{key.upper()}:")
+            DetoxicoLogger.start(f"{table.upper()}:")
             start = time.perf_counter()
 
-            install = self.build_install_command(val)
-            run = self.build_run_command(key, val)
+            install = self._build_install_command(table_entries)
+            run = self._build_run_command(table, table_entries)
+
+            if not run:
+                self.is_detox_successful = False
+                return
 
             cmd = f"source {self.TMP_VENV}/bin/activate"
             if install:
                 cmd += f" && {install}"
             cmd += f" && {run}"
 
-            is_successful = self.run_subprocess(cmd)
+            is_successful = self._run_subprocess(cmd)
 
             if not is_successful:
-                DetoxicoLogger.error(f"{key.upper()} failed")
-                self.failed_jobs.append(key)
+                self.failed_jobs.append(table)
+                DetoxicoLogger.error(f"{table.upper()} failed")
             else:
                 stop = time.perf_counter()
-                DetoxicoLogger.success(f"{key.upper()} succeeded! Took:  {stop - start}")
-            is_detox_successful &= is_successful
+                DetoxicoLogger.success(f"{table.upper()} succeeded! Took:  {stop - start}")
+                self.successful_jobs.append(table)
+            self.is_detox_successful &= is_successful
         DetoxicoLogger.log("#########################################")
-        return is_detox_successful
 
+    # build shell commands
     @staticmethod
-    def build_install_command(val):  # TODO: rename 'val'
-        deps = val.get("dependencies", None)
+    def _build_install_command(table_entries):
+        deps = table_entries.get("dependencies", None)
         if not deps:
             return
 
@@ -70,13 +168,15 @@ class DetoxRunner:
         install = "pip install " + install
         return install
 
-    # @staticmethod
-    def build_run_command(self, key, val):  # TODO: rename 'val'
+    def _build_run_command(self, table, table_entries):
         run = ""
-        cmds = val.get("commands", None)
+        cmds = table_entries.get("commands", None)
         if not cmds:
-            self.failed_jobs.append(key)  # TODO: move outside
-            raise KeyError(f"'commands' in '{key}' table cannot be empty or missing")
+            self.failed_jobs.append(table)
+            DetoxicoLogger.error(
+                f"Encountered error: 'commands' in '{table}' table cannot be empty or missing"
+            )
+            return
 
         if isinstance(cmds, list):
             run = " && ".join(cmds)
@@ -84,21 +184,9 @@ class DetoxRunner:
             run += f"{cmds}"
         return run
 
-    def read_file(self):
-        if os.path.exists(self.CONFIG_FILE) is False:
-            return False
-        if os.path.getsize(self.CONFIG_FILE) == 0:
-            return False
-
-        with open(self.CONFIG_FILE, "rb") as f:
-            self.data = tomllib.load(f)
-        # if file contains only comments
-        if not self.data:
-            return False
-        return True
-
+    # execute shell commands
     @staticmethod
-    def run_subprocess(run):
+    def _run_subprocess(run):
         is_successful = True
         with subprocess.Popen(
             run, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, executable="/bin/bash"
@@ -113,62 +201,6 @@ class DetoxRunner:
                     if "error" in text.lower():
                         is_successful = False
                 elif err:
-                    DetoxicoLogger.error(err, end="", flush=True)
                     is_successful = False
+                    DetoxicoLogger.error(err, end="", flush=True)
         return is_successful
-
-    def setup(self):
-        DetoxicoLogger.log("Creating venv...")
-        prepare = f"python3 -m venv {self.TMP_VENV}"
-        is_successful = self.run_subprocess(prepare)
-        if not is_successful:
-            DetoxicoLogger.error("Something went wrong :(")  # TODO: change message
-            self.failed_jobs.append("setup")
-            return False
-        return True
-
-    def teardown(self):
-        teardown = f"rm -rf {self.TMP_VENV}"
-        DetoxicoLogger.log("Removing venv...")
-        is_successful = self.run_subprocess(teardown)
-        if not is_successful:
-            DetoxicoLogger.error("Something went wrong :(")  # TODO: change message
-            self.failed_jobs.append("teardown")
-            return False
-        return True
-
-
-def main():
-    global_start = time.perf_counter()
-    DetoxicoLogger.info("Detoxing begins:")
-
-    runner = DetoxRunner()
-    is_toml_file_valid = runner.read_file()
-    if not is_toml_file_valid:
-        DetoxicoLogger.fail("Detoxing failed: missing or empty detox.toml file")
-        # return
-        sys.exit(1)
-
-    # TODO: flags could be class attributes
-    is_setup_successful = runner.setup()
-    # TODO: if creating venv fails we should exit -> remove also from final check if successful run
-
-    try:
-        is_detox_successful = runner.detox()
-    except (KeyError, Exception) as e:
-        DetoxicoLogger.error(f"Encountered error: {e}")
-        is_detox_successful = False
-
-    is_teardown_successful = runner.teardown()
-
-    global_stop = time.perf_counter()
-    if is_setup_successful and is_detox_successful and is_teardown_successful:
-        DetoxicoLogger.info(f"All jobs succeeded! {runner.all_jobs}")
-        DetoxicoLogger.info(f"Detoxing took: {global_stop - global_start}")
-    else:
-        # TODO: display which jobs were skipped due to raised exception
-        DetoxicoLogger.fail(f"Unsuccessful detoxing took: {global_stop - global_start}")
-        DetoxicoLogger.fail(f"Failed jobs: {runner.failed_jobs}")
-        DetoxicoLogger.fail(
-            f"Successful jobs: {[x for x in runner.all_jobs if x not in runner.failed_jobs]}"
-        )
